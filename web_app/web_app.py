bf4ba8ce67bee582c318edb6903c3791b8983b91
Yakir Orkabi <yakir@hexa3d.io>
2023-12-19 14:09:27 +0200

    fix - webapp

--- web_app/web_app.py
+++ web_app/web_app.py
@@ -1,17 +1,29 @@
-#pip install flask & jsonify & requests
+#Run -  pip install -r requirements.txt Before 
 
 from flask import Flask , jsonify
 import requests
+
 app = Flask(__name__)
 
 @app.route("/", methods=['GET',])
 def home():
     url = "https://official-joke-api.appspot.com/random_joke"
-    response = requests.get(url)
+    try:
+        response = requests.get(url, timeout=1)
+        response.raise_for_status() 
+    except requests.exceptions.HTTPError as http_err:
+        print("HTTP Error") 
+        print(http_err.args[0]) 
+    except requests.exceptions.RequestException as req_err:  
+        print("Exception request") 
+    except requests.exceptions.ReadTimeout as time_err: 
+        print("Time out") 
+    
     response_json = response.json()
     ID_VALUE = response_json.get("id")
     PUNCHLINE_VALUE = response_json.get("punchline")
-    return jsonify({"ID: ": ID_VALUE}, {"Punchline: ": PUNCHLINE_VALUE})
+
+    return jsonify({"ID": ID_VALUE, "Punchline": PUNCHLINE_VALUE})
     
 if __name__ == "__main__":
     app.run(debug=True, port=3001) 
